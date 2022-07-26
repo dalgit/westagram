4a459b1c64b97323ad6d25c030ae30761718f243
junki <junki646@gmail.com>
2022-07-26 17:30:16 +0900

    메인피드 구현, 어사이드바 부분 구현

--- js/login.js
+++ js/login.js
@@ -5,5 +5,7 @@ const loginButton = document.getElementById('loginButton')
 window.addEventListener("keyup",()=>{
     if(id.value.length>0 && password.value.length>0){
         loginButton.style.backgroundColor = '#0095F6';
+    } else if(id.value.length===0 || password.value.length===0){
+        loginButton.style.backgroundColor = '#C4E1FB';
     }
 })
